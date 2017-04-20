d01c2ab982f1fcc25f243bfe61789a7bca8fef27
William Oldham <wegg7250@gmail.com>
2017-04-20 21:07:27 +0100

    Task 1

---
+++ Little book of programming challenges/task1.rb
@@ -0,0 +1,4 @@
+puts 'Welcome to my joke program! A joke will be displayed and you can press enter to display the punchline!'
+puts 'What do you call an alligator wearing a vest?'
+gets
+puts 'An investigator!'
